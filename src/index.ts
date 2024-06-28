8bc9a122792c20af93b3c6abed047d41885edb2d
Rishabh Bhardwaj <rishabhb932@gmail.com>
2024-06-28 13:22:30 +0530

    Test Husky

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import http, { IncomingMessage, ServerResponse } from "http";
 import logger from './logger';
 
 export const server = http.createServer(
-  (req, res: ServerResponse) => {
+  (req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
